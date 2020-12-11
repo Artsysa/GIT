8c59fceb009022b8c56d351a1c02c2be1d01cdd4
liyouqi <youqi.li@ly.com>
2020-12-11 15:53:44 +0800

    '1.0.1f'

--- src/main/java/com/lyq/git/GitApplication.java
+++ src/main/java/com/lyq/git/GitApplication.java
@@ -8,9 +8,6 @@ public class GitApplication {
 
     public static void main(String[] args) {
         int i = 1;
-        for (int j = 0; j < 2; j++) {
-            i = i++;
-        }
         System.out.println(i);
         SpringApplication.run(GitApplication.class, args);
     }
